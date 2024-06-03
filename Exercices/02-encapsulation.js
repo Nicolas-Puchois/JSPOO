d914649a6c302959ee081e0f8f07c492c7f30b7a
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-06-03 15:19:43 +0200

    feat(02-encapsulation.js): add file

---
+++ Exercices/02-encapsulation.js
@@ -0,0 +1,11 @@
+/*
+Exercice 2 : Encapsulation
+
+Objectif : Appliquer le concept d'encapsulation.
+Consignes
+
+    Modifiez la classe Voiture pour que ses attributs soient privés.
+    Ajoutez des getters et des setters pour chaques attributs.
+    Créer une instance de Voiture et modifiez en la couleur.
+*/
+
