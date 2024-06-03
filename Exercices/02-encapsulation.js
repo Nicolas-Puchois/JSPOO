30688f06b2669500fdbab2c70a865bfea9603cae
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-06-03 16:00:43 +0200

    feat(02-encapsulation.js): add class "Voiture"

--- Exercices/02-encapsulation.js
+++ Exercices/02-encapsulation.js
@@ -9,3 +9,38 @@ Consignes
     Créer une instance de Voiture et modifiez en la couleur.
 */
 
+class Voiture {
+  #marque;
+  #modele;
+  #annee;
+  #couleur;
+  constructor() {}
+
+  setMarque(marque) {
+    this.#marque = marque;
+  }
+
+  setModele(modele) {
+    this.#modele = modele;
+  }
+
+  setAnnee(annee) {
+    this.#annee = annee;
+  }
+
+  setCouleur(couleur) {
+    this.#couleur = couleur;
+  }
+
+  afficherDetails() {
+    console.log(this.#marque, this.#modele, this.#annee, this.#couleur);
+  }
+}
+
+const voiture1 = new Voiture();
+
+voiture1.setMarque('Renault');
+voiture1.setModele('Twingo');
+voiture1.setAnnee(2019);
+voiture1.setCouleur('Blanche');
+voiture1.afficherDetails();
