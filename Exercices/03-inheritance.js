e4316bfea95a4694be2bb446cba930ec8e0f3e8a
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-06-04 09:59:47 +0200

    feat(03-inheritance.js): add object "Vehicule" and "Voiture"

--- Exercices/03-inheritance.js
+++ Exercices/03-inheritance.js
@@ -5,8 +5,33 @@ Objectif : Comprendre l'héritage entre classes.
 Consignes
 
     Créez une classe Vehicule avec les attributs marque, modele, et annee.
-    Faites en sorte que la classe Voiture hérite de la classe Vehicule et possède l'attribut couleur (non présent dans la classe Vehicule).
+    Faites en sorte que la classe Voiture hérite de la classe Vehicule et
+     possède l'attribut couleur (non présent dans la classe Vehicule).
 
 */
 
 'use strict';
+
+class Vehicule {
+    constructor(marque, modele, annee) {
+        this.marque = marque;
+        this.modele = modele;
+        this.annee = annee;
+    }
+}
+
+class Voiture extends Vehicule {
+    constructor(marque, modele, annee, couleur) {
+        super(marque, modele, annee);
+        this.couleur = couleur;
+    }
+
+    afficherDetails() {
+        console.log(this.marque, this.modele, this.annee, this.couleur);
+    }
+}
+
+
+const voiture1 = new Voiture ('Renault', 'Twingo', 2019, 'Blanche');
+
+voiture1.afficherDetails();
