1bdb2945b510c8f62b5994d266d77326275a9564
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-06-04 09:49:41 +0200

    feat(03-inheritance.js): add file

---
+++ Exercices/03-inheritance.js
@@ -0,0 +1,12 @@
+/*
+Exercice 3 : Héritage
+
+Objectif : Comprendre l'héritage entre classes.
+Consignes
+
+    Créez une classe Vehicule avec les attributs marque, modele, et annee.
+    Faites en sorte que la classe Voiture hérite de la classe Vehicule et possède l'attribut couleur (non présent dans la classe Vehicule).
+
+*/
+
+'use strict';
